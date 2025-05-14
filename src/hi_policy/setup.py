1623b1d5a2721f276f4352b46069d4e250180c99
henricus <henricus0973@korea.ac.kr>
2025-05-15 02:09:17 +0900

    global_path_planning package : Initialize

---
+++ src/hi_policy/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'hi_policy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/hi_level_launch.py']),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='henricus',
+    maintainer_email='henricus0973@korea.ac.kr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'global_path_planner = hi_policy.global_path_planner:main',
+        ],
+    },
+)
