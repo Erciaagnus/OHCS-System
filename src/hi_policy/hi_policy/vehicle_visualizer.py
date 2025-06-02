8798183bca02a3f4879e0b63387ebfa96f14a584
JeongHyeok  Lim <henricus0973@korea.ac.kr>
2025-06-02 18:11:38 +0200

    added visualized parts of users location

--- src/hi_policy/hi_policy/vehicle_visualizer.py
+++ src/hi_policy/hi_policy/vehicle_visualizer.py
@@ -5,6 +5,8 @@ from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
 from hlc_interfaces.msg import ChargerState
+from typing import List, Tuple, Dict
+from parking_world.rail_visualizer import RailNode, RailSegment, RailMap
 class ChargerVisualizer(Node):
     def __init__(self):
         super().__init__('charger_visualizer')
@@ -41,7 +43,31 @@ class ChargerVisualizer(Node):
 
             marker_array.markers.append(marker)
         self.publisher_.publish(marker_array)
+class GoalVisualizer(Node):
+    def __init__(self):
+        super().__init__('user_visualizer')
+        self.publisher = self.create_publisher(Marker, '/user_markers', 10)
 
+    def publish_goals(self, goal_nodes: List[str], rail_map: RailMap):
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.ns = "goal_nodes"
+        marker.id = 0
+        marker.type = Marker.SPHERE_LIST
+        marker.action = Marker.ADD
+        marker.scale.x = 0.4
+        marker.scale.y = 0.4
+        marker.scale.z = 0.4
+        marker.color = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)  # 초록색
+
+        marker.points = []
+        for node_id in goal_nodes:
+            node = rail_map.nodes.get(node_id)
+            if node:
+                marker.points.append(Point(x=node.x, y=node.y, z=2))
+
+        self.publisher.publish(marker)
 def main(args=None):
     rclpy.init(args=args)
     node = ChargerVisualizer()
